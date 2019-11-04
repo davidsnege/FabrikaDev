4eb593ec2b7a4eba837cc4cb68307bb5385e26b6
David Rodriguez Belleti Snege <david.snege@gmail.com>
2019-11-04 11:17:00 +0100

    Atualizado Send Email
    agora temos um editor de texto para o envio de e-mails em html

--- mail/send_mail.php
+++ mail/send_mail.php
@@ -1,3 +1,34 @@
 <?php
+$desde = $_POST['desde'];
+$para = $_POST['para'];
+$assunto = $_POST['assunto'];
+$texto = htmlspecialchars($_POST['texto']);
 
+// título
+$titulo = 'Recordatorio de cumpleaños para Agosto';
+
+// mensaje
+$mensaje = '
+<html>
+<head>
+  <title>Email desde Mi PHP</title>
+</head>
+<body>
+    '.$texto.'
+</body>
+</html>
+';
+
+// Para enviar un correo HTML, debe establecerse la cabecera Content-type
+$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Cabeceras adicionales
+$cabeceras .= 'To:  <'.$para.'>' . "\r\n";
+$cabeceras .= 'From: David <'.$desde.'>' . "\r\n";
+// $cabeceras .= 'Cc: birthdayarchive@example.com' . "\r\n";
+// $cabeceras .= 'Bcc: birthdaycheck@example.com' . "\r\n";
+
+// Enviarlo
+mail($para, $assunto, $mensaje, $cabeceras);
 ?>
