34359b9ba36a8148d2cc8f5ff7966c779fb8a0ca
David Rodriguez Belleti Snege <david.snege@gmail.com>
2019-12-23 11:56:30 +0100

    Ayuda para los fopen

--- edita_txt/generador.php
+++ edita_txt/generador.php
@@ -79,14 +79,21 @@
 
           // HELP - Descrição PT-BR
 
-          // 'r'	Abre somente para leitura; coloca o ponteiro do arquivo no começo do arquivo.
-          // 'r+'	Abre para leitura e escrita; coloca o ponteiro do arquivo no começo do arquivo.
-          // 'w'	Abre somente para escrita; coloca o ponteiro do arquivo no começo do arquivo e reduz o comprimento do arquivo para zero. Se o arquivo não existir, tenta criá-lo.
-          // 'w+'	Abre para leitura e escrita; coloca o ponteiro do arquivo no começo do arquivo e reduz o comprimento do arquivo para zero. Se o arquivo não existir, tenta criá-lo.
-          // 'a'	Abre somente para escrita; coloca o ponteiro do arquivo no final do arquivo. Se o arquivo não existir, tenta criá-lo.
-          // 'a+'	Abre para leitura e escrita; coloca o ponteiro do arquivo no final do arquivo. Se o arquivo não existir, tenta criá-lo.
-          // 'x'	Cria e abre o arquivo somente para escrita; coloca o ponteiro no começo do arquivo. Se o arquivo já existir, a chamada a fopen() falhará, retornando FALSE e gerando um erro de nível E_WARNING. Se o arquivo não existir, tenta criá-lo. Isto é equivalente a especificar as flags O_EXCL|O_CREAT para a chamada de sistema open(2).
-          // 'x+'	Cria e abre o arquivo para leitura e escrita; coloca o ponteiro no começo do arquivo. Se o arquivo já existir, a chamada a fopen() falhará, retornando FALSE e gerando um erro de nível E_WARNING. Se o arquivo não existir, tenta criá-lo. Isto é equivalente a especificar as flags O_EXCL|O_CREAT para a chamada de sistema open(2).
+          ### Modos de Abertura de um arquivo com fopen()
+
+
+          // 'r'	Apertura para sólo lectura; coloca el puntero al fichero al principio del fichero.
+          // 'r+'	Apertura para lectura y escritura; coloca el puntero al fichero al principio del fichero.
+          // 'w'	Apertura para sólo escritura; coloca el puntero al fichero al principio del fichero y trunca el fichero a longitud cero. Si el fichero no existe se intenta crear.
+          // 'w+'	Apertura para lectura y escritura; coloca el puntero al fichero al principio del fichero y trunca el fichero a longitud cero. Si el fichero no existe se intenta crear.
+          // 'a'	Apertura para sólo escritura; coloca el puntero del fichero al final del mismo. Si el fichero no existe, se intenta crear. En este modo, fseek() solamente afecta a la posición de lectura; las lecturas siempre son pospuestas.
+          // 'a+'	Apertura para lectura y escritura; coloca el puntero del fichero al final del mismo. Si el fichero no existe, se intenta crear. En este modo, fseek() no tiene efecto, las escrituras siempre son pospuestas.
+          // 'x'	Creación y apertura para sólo escritura; coloca el puntero del fichero al principio del mismo. Si el fichero ya existe, la llamada a fopen() fallará devolviendo FALSE y generando un error de nivel E_WARNING. Si el fichero no existe se intenta crear. Esto es equivalente a especificar las banderas O_EXCL|O_CREAT para la llamada al sistema de open(2) subyacente.
+          // 'x+'	Creación y apertura para lectura y escritura; de otro modo tiene el mismo comportamiento que 'x'.
+          // 'c'	Abrir el fichero para sólo escritura. Si el fichero no existe, se crea. Si existe no es truncado (a diferencia de 'w'), ni la llamada a esta función falla (como en el caso con 'x'). El puntero al fichero se posiciona en el principio del fichero. Esto puede ser útil si se desea obtener un bloqueo asistido (véase flock()) antes de intentar modificar el fichero, ya que al usar 'w' se podría truncar el fichero antes de haber obtenido el bloqueo (si se desea truncar el fichero, se puede usar ftruncate() después de solicitar el bloqueo).
+          // 'c+'	Abrir el fichero para lectura y escritura; de otro modo tiene el mismo comportamiento que 'c'.
+          // 'e'	Establecer la bandera 'close-on-exec' en el descriptor de fichero abierto. Disponible solamente en PHP compilado en sistemas que se ajustan a POSIX.1-2008.
+
 
           // fclose() - Fecha um ponteiro de arquivo aberto
           // fgets() - Lê uma linha de um ponteiro de arquivo
@@ -102,14 +109,22 @@
 
           // HELP - Description EN
 
-          // 'r' Opens for read only; place the file pointer at the beginning of the file.
-          // 'r +' Opens for reading and writing; place the file pointer at the beginning of the file.
-          // 'w' Open for writing only; places the file pointer at the beginning of the file and reduces the file length to zero. If the file does not exist, try to create it.
-          // 'w +' Opens for reading and writing; places the file pointer at the beginning of the file and reduces the file length to zero. If the file does not exist, try to create it.
-          // 'a' Opens for writing only; place the file pointer at the end of the file. If the file does not exist, try to create it.
-          // 'a +' Opens for reading and writing; place the file pointer at the end of the file. If the file does not exist, try to create it.
-          // 'x' Create and open the write-only file; place the pointer at the beginning of the file. If the file already exists, the call to fopen () will fail, returning FALSE and generating an E_WARNING level error. If the file does not exist, try to create it. This is equivalent to specifying the O_EXCL | O_CREAT flags for the open (2) system call.
-          // 'x +' Create and open the file for reading and writing; place the pointer at the beginning of the file. If the file already exists, the call to fopen () will fail, returning FALSE and generating an E_WARNING level error. If the file does not exist, try to create it. This is equivalent to specifying the O_EXCL | O_CREAT flags for the open (2) system call.
+
+          ### Modos de Abertura de um arquivo com fopen()
+
+
+          // 'r'	Apertura para sólo lectura; coloca el puntero al fichero al principio del fichero.
+          // 'r+'	Apertura para lectura y escritura; coloca el puntero al fichero al principio del fichero.
+          // 'w'	Apertura para sólo escritura; coloca el puntero al fichero al principio del fichero y trunca el fichero a longitud cero. Si el fichero no existe se intenta crear.
+          // 'w+'	Apertura para lectura y escritura; coloca el puntero al fichero al principio del fichero y trunca el fichero a longitud cero. Si el fichero no existe se intenta crear.
+          // 'a'	Apertura para sólo escritura; coloca el puntero del fichero al final del mismo. Si el fichero no existe, se intenta crear. En este modo, fseek() solamente afecta a la posición de lectura; las lecturas siempre son pospuestas.
+          // 'a+'	Apertura para lectura y escritura; coloca el puntero del fichero al final del mismo. Si el fichero no existe, se intenta crear. En este modo, fseek() no tiene efecto, las escrituras siempre son pospuestas.
+          // 'x'	Creación y apertura para sólo escritura; coloca el puntero del fichero al principio del mismo. Si el fichero ya existe, la llamada a fopen() fallará devolviendo FALSE y generando un error de nivel E_WARNING. Si el fichero no existe se intenta crear. Esto es equivalente a especificar las banderas O_EXCL|O_CREAT para la llamada al sistema de open(2) subyacente.
+          // 'x+'	Creación y apertura para lectura y escritura; de otro modo tiene el mismo comportamiento que 'x'.
+          // 'c'	Abrir el fichero para sólo escritura. Si el fichero no existe, se crea. Si existe no es truncado (a diferencia de 'w'), ni la llamada a esta función falla (como en el caso con 'x'). El puntero al fichero se posiciona en el principio del fichero. Esto puede ser útil si se desea obtener un bloqueo asistido (véase flock()) antes de intentar modificar el fichero, ya que al usar 'w' se podría truncar el fichero antes de haber obtenido el bloqueo (si se desea truncar el fichero, se puede usar ftruncate() después de solicitar el bloqueo).
+          // 'c+'	Abrir el fichero para lectura y escritura; de otro modo tiene el mismo comportamiento que 'c'.
+          // 'e'	Establecer la bandera 'close-on-exec' en el descriptor de fichero abierto. Disponible solamente en PHP compilado en sistemas que se ajustan a POSIX.1-2008.
+
 
           // fclose() - Closes an open file pointer
           // fgets() - Reads a line from a file pointer
