8caf60de659a7fa1a365f4e37257d0133b672dc3
David Rodriguez Belleti Snege <david.snege@gmail.com>
2020-01-13 17:35:12 +0100

    Arreglando las cosas
    Tenia errores, aun no he podido testear el SSH

--- ftp_php/ftp.php
+++ ftp_php/ftp.php
@@ -1,14 +1,22 @@
 <?php
 
 // abrir un archivo para su lectura
-$file = 'somefile.txt';
+$file = 'test.txt';
 $fp = fopen($file, 'r');
 
+
+$ftp_server = "185.66.41.48";
+$ftp_user_name = "davidsne";
+$ftp_user_pass = "gnMH29aa";
+$ftp_dirInt = "/web";
+
 // establecer una conexión básica
-$conn_id = ftp_connect($ftp_server);
+$conn_id = ftp_connect($ftp_server) or die("No se pudo conectar a $ftp_server");
 
 // iniciar sesion con nombre de usuario y contraseña
 $login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
+// Change the dir
+ftp_chdir($conn_id, $ftp_dirInt);
 
 // intentar cargar $file
 if (ftp_fput($conn_id, $file, $fp, FTP_ASCII)) {
--- ftp_php/ftp_conect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$ftp_server = "ftp.example.com";
-
-// establecer una conexión o finalizarla
-$conn_id = ftp_connect($ftp_server) or die("No se pudo conectar a $ftp_server");
-
-?>
