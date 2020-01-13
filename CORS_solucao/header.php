3eb3989c2e37440203919b289a74fe270eea6966
David Rodriguez Belleti Snege <david.snege@gmail.com>
2020-01-13 13:55:57 +0100

    Update header.php

--- CORS_solucao/header.php
+++ CORS_solucao/header.php
@@ -3,4 +3,5 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 //https://desarrolloweb.com/articulos/solucion-problemas-api-php-cors-post.html
+//
 ?>
