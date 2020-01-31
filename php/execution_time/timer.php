573d99539539f697c1b3731c4f815720c09e3627
davidrodriguez <david.snege@gmail.com>
2020-01-31 09:22:40 +0100

    Update timer.php
    Comentarios

--- php/execution_time/timer.php
+++ php/execution_time/timer.php
@@ -3,6 +3,10 @@
 
 // Coloque todo seu código e queryes aqui e saiba ao final quanto demorou para executar o arquivo
 
+// 1. Se você colocar seu time_start dentro de uma função termine ele dentro de uma função
+// 2. Se você colocar ele no começo de um script termine ele no final do arquivo
+
+// Obs. Muito util para saber tempos de execução de querys ou whiles e foreach longos, use também para leitura de json
 
 // Você pode usar o contador final no Fim do arquivo ou depois de cada function dentro para saber os tempos separados de cada função
 
