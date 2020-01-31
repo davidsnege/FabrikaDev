5b7febddf20a75024ec8335d0e9914128bb6f390
davidrodriguez <davidrodriguez@thelis.es>
2020-01-31 09:13:47 +0100

    Update header.php
    Comentários

--- php/cors_solucao/header.php
+++ php/cors_solucao/header.php
@@ -1,4 +1,9 @@
 <?php
+
+// Si você tiver problemas com CORS no console usar alguma destas técnicas pode te ajudar
+// O melhor é identificar a CDN ou de onde vem o erro CORS e arrumar
+// As opções abaixo são somente gambiarras
+
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
