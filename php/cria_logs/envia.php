ffc16b21834f54413bab274c0963e8271268b313
davidrodriguez <david.snege@gmail.com>
2020-02-07 17:40:56 +0100

    Criador de Logs para sites
    Ver como funciona primeiro.....
    
    Inclua o arquivo no seu script
    chame a funcao dentro de seus ifs de erros
    personalize a mensagem de erro

---
+++ php/cria_logs/envia.php
@@ -0,0 +1,34 @@
+<?php 
+    //╔══════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+    //║  Envia os logs pela função enviaLog() para o arquivo que grava no TXT
+    //║  Created By David Snege
+    //╚══════════════════════════════════════════════════════════════════════════════════════════════════════════╝
+    //╔══════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+        function enviaLog($mensagem){
+
+            $content = http_build_query(array(
+            'mensagem_log' => $mensagem
+            ));
+
+            $context = stream_context_create(array(
+            'http' => array(
+            'header' => "Content-Type: application/x-www-form-urlencoded\r\n".
+                                "Content-Length: ".strlen($content)."\r\n".
+                                "User-Agent:MyAgent/1.0\r\n",
+            'method' => 'POST',
+            'type' => 'application/x-www-form-urlencoded',
+            'content' => $content,
+            )
+            ));
+
+        $result = file_get_contents('http://localhost/github/FabrikaDev/php/cria_logs/generador_logs.php', null, $context);
+
+        }
+
+        // Teste de passar mensagem para a funcao de gravar no generador
+        enviaLog("testando passar mensagem pra funcao");
+
+        // Incluir este arquivo com include dentro de seu projeto no inicio
+        // Depois chamar a função enviaLog(); quando for necessario
+
+    //╚══════════════════════════════════════════════════════════════════════════════════════════════════════════╝
---
+++ php/cria_logs/generador_logs.php
@@ -0,0 +1,57 @@
+<?php
+    //╔══════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+    //║  Gerar Logs para site, Necessita do arquivo envia.php "leia mais en envia.php"
+    //║  Created By David Snege
+    //╚══════════════════════════════════════════════════════════════════════════════════════════════════════════╝
+    //╔══════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+            // Declaramos variables
+            if(isset($_POST['mensagem_log'])){
+            
+
+            // Time monitor
+            $time_start = microtime(true);
+
+            // Geramos ID de LOG
+            function generateRandomString($length = 128) {
+                $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                $charactersLength = strlen($characters);
+                $randomString = '';
+                for ($i = 0; $i < $length; $i++) {
+                    $randomString .= $characters[rand(0, $charactersLength - 1)];
+                }
+                return $randomString;
+            }
+
+            // Armazenamos o valor aqui em value
+            $value = "LOG: ". generateRandomString() . "\n";
+
+            // Paramos de contar o tempo aqui
+            $time_end = microtime(true);
+			$execution_time = ($time_end - $time_start);
+
+            // Contruimos a mensagem de log
+            $value .= "Mensagem de Log:    " .$_POST['mensagem_log']. " \n";
+            $value .= "Total Time:         " .intval($time_end). "m secs \n";
+            $value .= "Data de Log:        " .date("d-m-Y") . " \n";
+            $value .= "Hora de Log:        " .date("h:i:sa") . " \n";
+
+            // Si exhiste archivo
+            $filename = "log.txt";
+            if (file_exists($filename)) {
+            // echo "El archivo $filename ehxiste <br>";
+            } else {
+                echo "El archivo $filename no ehxiste <br>";
+            }
+            // Cria Archivo si no hay
+            $arquivo = fopen("log.txt","a", -100);
+            // Verifico si esta creado
+            if ($arquivo == false) die("Não foi possível criar o arquivo. <br>");
+            // Escribimos en el archivo
+            fwrite($arquivo, $value."\n"); // Para saltar a linha tem que estar o "\n" entre aspas como string
+            // Cerramos el archivo despues de escribir
+            fclose($arquivo);
+            exit;
+
+            }
+    //╚══════════════════════════════════════════════════════════════════════════════════════════════════════════╝
+?>
