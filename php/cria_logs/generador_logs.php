d5ddd3554017e81d6681684bf2ca1dee7a66f7f3
davidrodriguez <david.snege@gmail.com>
2020-02-07 17:44:40 +0100

    Ver comentario para melhorar
    Dar opções de:
    
    (gravar TXT), (gravar TXT, enviar Email), (gravar TXT, enviar Email, gravar no BD)

--- php/cria_logs/generador_logs.php
+++ php/cria_logs/generador_logs.php
@@ -52,6 +52,12 @@
             fclose($arquivo);
             exit;
 
+            
+            // Melhorar este arquivo com a opção de enviar um email de log para o admin
+            // Melhoar este arquivo com a opção de gravar na base de dados se necessario
+            // Estas opções tem que ser passadas no argumento como true ou false e entao este escript tem que
+            // decidir o que fazer com o log (gravar TXT), (gravar TXT, enviar Email), (gravar TXT, enviar Email, gravar no BD)
+
             }
     //╚══════════════════════════════════════════════════════════════════════════════════════════════════════════╝
 ?>
