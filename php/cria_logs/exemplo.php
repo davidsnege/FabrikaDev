4523b99d7f92652a3058e4a07eab45bd44fd5dd3
davidrodriguez <david.snege@gmail.com>
2020-02-10 13:30:50 +0100

    melhorias
    JOQUEMPO

--- php/cria_logs/exemplo.php
+++ php/cria_logs/exemplo.php
@@ -9,7 +9,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-<div class="container">
+<div class="container"><center>
 <?php
 
     //╔══════════════════════════════════════════════════════════════════════════════════════════════════════════╗
@@ -24,6 +24,10 @@
         $pedra ='Pedra';
         $papel ='Papel';
         $tesoura ='Tesoura';
+
+        
+        $puntoC =0;
+        $puntoH =0;
 
         if(!isset($_COOKIE["pontosHumano"]) || !isset($_COOKIE["pontosComputador"]))
         {
@@ -53,36 +57,72 @@
 
         }else{
 
-            $puntoC = '';
-            $puntoH = '';
-            
+
+            $computador = array("$pedra", "$papel","$tesoura");
+            $randIndex1 = array_rand($computador);
+
+
+                    if(isset($_POST['escolha'])){ // Si Humano escolhe uma opção vai pela escolha
+                        $humano = $_POST['escolha'];
+                        $randIndex2 = $_POST['escolha'];
+                    }else{
+                        $humano = array("$pedra", "$papel","$tesoura"); // Se não ele escolhe um random por F5
+                        $randIndex2 = array_rand($humano);
+                        $humano = $humano[$randIndex2];
+                    }
+
+                    // porque temos isso
+                    $puntoC = $_COOKIE["pontosComputador"];
+                    $puntoH = $_COOKIE["pontosHumano"];
+
+           
             echo '
             <div class="row">
             <div class="col-md-4">
 
-                <h5>Computador</h5>
+                <h5>C</h5>
 
-            '.$_COOKIE["pontosComputador"].'
             </div>
             ';
-
-        $computador = array("$pedra", "$papel","$tesoura");
-        $randIndex1 = array_rand($computador);
-
-        if(isset($_POST['escolha'])){ // Si Humano escolhe uma opção vai pela escolha
-            $humano = $_POST['escolha'];
-            $randIndex2 = $_POST['escolha'];
-        }else{
-            $humano = array("$pedra", "$papel","$tesoura"); // Se não ele escolhe um random por F5
-            $randIndex2 = array_rand($humano);
-            $humano = $humano[$randIndex2];
-        }
-
-
 
         echo '
         <div class="col-md-4">
         ';
+
+                    function confereGanha(){
+                        $puntoC = $_COOKIE["pontosComputador"];
+                        $puntoH = $_COOKIE["pontosHumano"];
+                        // Si chegamos a 5 pontos de algum dos lados entao voltamos a zero e começamos de novo
+                        if(isset($_COOKIE["pontosComputador"])){
+                            if($_COOKIE["pontosComputador"] >= 5 || $_COOKIE["pontosHumano"] >= 5){
+                                
+                                if($_COOKIE["pontosComputador"] > $_COOKIE["pontosHumano"]){
+                                    echo "Computador Ganhou";
+                                    setcookie("pontosComputador", 0, time()+3600);
+                                    setcookie("pontosHumano", 0, time()+3600);
+                                    ++$puntoC;
+                                    ++$puntoH;
+
+                                }elseif($_COOKIE["pontosComputador"] < $_COOKIE["pontosHumano"]){
+                                    echo "Humano Ganhou";
+                                    setcookie("pontosComputador", 0, time()+3600);
+                                    setcookie("pontosHumano", 0, time()+3600);
+                                    ++$puntoC;
+                                    ++$puntoH;
+
+                                }else{
+                                    echo "Que bonitinho, Empataram";
+                                    setcookie("pontosComputador", 0, time()+3600);
+                                    setcookie("pontosHumano", 0, time()+3600);
+                                    ++$puntoC;
+                                    ++$puntoH;
+
+                                }
+                            }
+                        }
+                    }
+
+            //
             if ($computador[$randIndex1] === $humano){
                 echo "Temos um empate";
                 $puntoC = $_COOKIE["pontosComputador"];
@@ -145,14 +185,15 @@
             </div>
 
             <div class="col-md-4">
-            <h5>Humano</h5>
-            '.$_COOKIE["pontosHumano"].'
+            <h5>H</h5>
             </div>
 
             </div>
 
             ';
+
         }
+
 
     //╚══════════════════════════════════════════════════════════════════════════════════════════════════════════╝
 ?>
@@ -183,7 +224,43 @@
 
             </div>
 
-</div>
+
+
+
+            <div class="row">
+
+                <div class="col-sm">
+                    <?php echo $_COOKIE["pontosComputador"]; ?>
+                </div>
+
+                    <div class="col-sm">
+                        x   
+                    </div>
+
+                <div class="col-sm">
+                    <?php echo $_COOKIE["pontosHumano"]; ?>
+                </div>
+                
+            </div>            
+            
+            
+            <div class="row">
+
+                <div class="col-sm">
+                    
+                </div>
+
+                    <div class="col-sm">
+                        <?php confereGanha(); ?>
+                    </div>
+
+                <div class="col-sm">
+                    
+                </div>
+                
+            </div>
+
+</div></center>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
