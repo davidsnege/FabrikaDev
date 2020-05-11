8b472e3a1fa8f6ab1c0fc684b70c776195c7fb2b
davidsnege <david.snege@gmail.com>
2020-05-11 17:49:31 +0200

    Imprimimor imagem
    Agora criamos a imagem, e podemos seguir avançando

--- php/cor_imagem/cor_pixel.php
+++ php/cor_imagem/cor_pixel.php
@@ -1,4 +1,7 @@
 <?php
+// Se nao colocamos o header nao podemos imprimir imagem com os recursos do php
+// Temos que usar isso para que retorne um tipo de imagem real.
+header ('Content-Type: image/png');
 
 // TODO 
 // saber o tipo de imagem para usar o imagecreate correto (extrair o tipo mime do array getimagesize)
@@ -17,10 +20,15 @@
 // criar um modelo pronto para analizar imagens obtidas em sequencia por telescopios e analizar pixels que se moveram
 // a partir disto detectar e marcar a imagem ou a sequencia como possivel NEO
 
-
+// Os tipos de imagens aceitos
+// IMAGETYPE_GIF => 'imagecreatefromgif',
+// IMAGETYPE_JPEG => 'imagecreatefromjpeg',
+// IMAGETYPE_PNG => 'imagecreatefrompng',
+// IMAGETYPE_WBMP => 'imagecreatefromwbmp',
+// IMAGETYPE_XBM => 'imagecreatefromwxbm',
 
 //Defnimos a rota da imagem
-$image = "stars.jpg";
+$image = "imgs/stars.jpg";
 
 // Cogemos el tamaño de la imagem para poder hacer un foreach y recorrer colores
 $size = getimagesize($image);
@@ -29,8 +37,11 @@ $height = $size['1'];
 $mime = $size['mime'];
 $bits = $size['bits'];
 
-// echo $height;
-// echo $width;
+// Confirguracao de formacao de imagem
+$bwwb = "wb";
+$sensitive = "180";
+$wColor = "000";
+$bColor = "255";
 
 // Definimos que tipo de comando vamos usar por tipo de imagem
 switch ($mime) {
@@ -48,48 +59,121 @@ switch ($mime) {
         break;
 }
 
-// Os tipos de imagens aceitos
-// IMAGETYPE_GIF => 'imagecreatefromgif',
-// IMAGETYPE_JPEG => 'imagecreatefromjpeg',
-// IMAGETYPE_PNG => 'imagecreatefrompng',
-// IMAGETYPE_WBMP => 'imagecreatefromwbmp',
-// IMAGETYPE_XBM => 'imagecreatefromwxbm',
+// definimos que X e Y são iguais a width e height para ser usado por imagecreatetruecolor
+$x = $width;
+$y = $height;
+
+// Usamos imagecreatetruecolor para definir o tamanho da imagem que vamos criar
+$gd = imagecreatetruecolor($x, $y);
+
+// Começamos a scanear os pixels da imagem buscando o que quremos e construindo outra imagem com o for.
 
 
-// Construimos uma imagem com caracteres para ver a previa do que queremos dentro do array de estrelas.
-    echo '<div class="container">';
+    // echo '<div class="container">'; // Usar para printar un texto
+
         for ($h=0; $h < $height; $h++) { 
-            echo '<div class="row">';
+
+    //         echo '<div class="row">'; // Usar para printar un texto
+
             for ($w=0; $w < $width; $w++) { 
 
+                // Aqui usamos as propriedades do PHP imgecolorat para definir onde estamos percorrendo neste momento. e definimos o valor da cor en rgb
                 $rgb = imagecolorat($im, $w, $h);
                 $r = ($rgb >> 16) & 0xFF;
                 $g = ($rgb >> 8) & 0xFF;
                 $b = $rgb & 0xFF;
 
-
-                // Aplicamos contraste de acordo com o RGB 
-                // Desta forma podemos ver somente as areas mais brancas ou mais negras da imagem
-                // Ver como fazer um toggle aqui para inverter as cores quando necessario
-                if($r && $g && $b <= 240){
-                    $r = 255;
-                    $g = 255;
-                    $b = 255;
-                }else{
-                    $r = 000;
-                    $g = 000;
-                    $b = 000;                    
+                // Definimos se queremos em BW ou en WB antes de aplicar o contraste. (podemos ir criando outros filtros utilizando estes cases e definindo outras cores)
+                switch ($bwwb) {
+                    case 'bw': // bw Preto e Branco
+                            if($r && $g && $b >= $sensitive){
+                                $r = $wColor;
+                                $g = $wColor;
+                                $b = $wColor;
+                            }else{
+                                $r = $bColor;
+                                $g = $bColor;
+                                $b = $bColor;                    
+                            }
+                    break;
+                    case 'wb': // wb Branco e Preto
+                            if($r && $g && $b >= $sensitive){
+                                $r = $bColor;
+                                $g = $bColor;
+                                $b = $bColor;
+                            }else{
+                                $r = $wColor;
+                                $g = $wColor;
+                                $b = $wColor;                    
+                            }
+                    break;
+                    case 'red': // bwc Preto e Branco Contraste Color
+                            if($r && $g && $b >= $sensitive){
+                                $r = 255;
+                                $g = 25;
+                                $b = 0;
+                            }else{
+                                $r = $r;
+                                $g = $g;
+                                $b = $b;                                  
+                            }
+                    break;                    
+                    case 'blue': // bwc Preto e Branco Contraste Color
+                            if($r && $g && $b >= $sensitive){
+                                $r = 0;
+                                $g = 67;
+                                $b = 255;
+                            }else{
+                                $r = $r;
+                                $g = $g;
+                                $b = $b;                                  
+                            }
+                    break;                    
+                    case 'green': // bwc Preto e Branco Contraste Color
+                            if($r && $g && $b >= $sensitive){
+                                $r = 000;
+                                $g = 193;
+                                $b = 33;
+                            }else{
+                                $r = $r;
+                                $g = $g;
+                                $b = $b;                                  
+                            }
+                    break;                  
                 }
 
 
+    // Usar para printar un texto
+    //             // Printamos Letras como se fossem pixels "♦" para ver alguma coisa, nos falta criar uma imagem real a partir desta informacao
+    //             // echo '<text id="w'.$w.'h'.$h.'" style="color:rgba('.$r.', '.$g.', '.$b.'); font-size: 6px; letter-spacing: -0px; line-height: 50%;">■</text>';
+    //             //  ALT 254 / 176 / 177 / 178 / 220 / 4 /
+    //             // ♦ // Usar para printar un texto
 
-                echo '<text id="w'.$w.'h'.$h.'" style="color:rgba('.$r.', '.$g.', '.$b.'); font-size: 4px; letter-spacing: -0px; line-height: 50%;">♦</text>';
-                //  ALT 254 / 176 / 177 / 178 / 220 / 4 /
+
+
+                        // Criamos a cor do pixel atual do for
+                        $color = imagecolorallocate($gd, $r, $g, $b); 
+                        // Criamos o array das coordenadas de cada pixel
+                        $esquinas[0] = array('x' => $w, 'y' =>  $h);
+                        // Setamos um Pixel da cor atual nas coordenadas atualis da imagem
+                        imagesetpixel($gd, round($x),round($y), $color);
+                        // Definimos que a usa a array acima 0 de esquinas
+                        $a = 0;
+                        // Criamos as coordenadas e a escala esta definida no  / 2 (podemos mudar isso.)
+                        $x = ($x + $esquinas[$a]['x']) / 2;
+                        $y = ($y + $esquinas[$a]['y']) / 2;
+
             }
-            echo '</div>';
+    //         echo '</div>'; // Usar para printar un texto
         }
-    echo '</div>';
+
+    // echo '</div>'; // Usar para printar un texto
 // **************************************************************
-    
+
+// Imprimimos a imagem usando o imagetype e o comando do php imagepng
+header('Content-Type: image/png');
+// Este comando imprime a imagem desde que o header esteja declarado corretamente
+imagepng($gd);
+
 
 ?>
